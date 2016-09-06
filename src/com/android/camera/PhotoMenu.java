amirzaidi/SnapdragonCamera

SnapdragonCamera: Selfie Mirror mode
Selfie mirror menu for front camera1.

Change-Id: I2e6845c9e25481106f51772b5d3dfd0a971a2be7
CRs-Fixed: 1050663

src/com/android/camera/PhotoMenu.java
@@ -191,7 +191,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_EXPOSURE,
                 CameraSettings.KEY_WHITE_BALANCE,
                 CameraSettings.KEY_QC_CHROMA_FLASH,
-                CameraSettings.KEY_REDEYE_REDUCTION
+                CameraSettings.KEY_REDEYE_REDUCTION,
+                CameraSettings.KEY_SELFIE_MIRROR
         };
 
         mOtherKeys2 = new String[] {
@@ -234,6 +235,7 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_MANUAL_EXPOSURE,
                 CameraSettings.KEY_MANUAL_WB,
                 CameraSettings.KEY_MANUAL_FOCUS,
+                CameraSettings.KEY_SELFIE_MIRROR,
                 SettingsManager.KEY_CAMERA2
         };
 

AmolAmrit/packages_apps_SnapdragonCamera

src/com/android/camera/PhotoMenu.java
@@ -187,7 +187,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_EXPOSURE,
                 CameraSettings.KEY_WHITE_BALANCE,
                 CameraSettings.KEY_QC_CHROMA_FLASH,
-                CameraSettings.KEY_REDEYE_REDUCTION
+                CameraSettings.KEY_REDEYE_REDUCTION,
+                CameraSettings.KEY_SELFIE_MIRROR
         };
 
         mOtherKeys2 = new String[] {
@@ -228,7 +229,8 @@ public class PhotoMenu extends MenuController
                 CameraSettings.KEY_AE_BRACKET_HDR,
                 CameraSettings.KEY_MANUAL_EXPOSURE,
                 CameraSettings.KEY_MANUAL_WB,
-                CameraSettings.KEY_MANUAL_FOCUS
+                CameraSettings.KEY_MANUAL_FOCUS,
+                CameraSettings.KEY_SELFIE_MIRROR
         };
 
         initSwitchItem(CameraSettings.KEY_CAMERA_ID, mFrontBackSwitcher);